27cfafaf514312a5dd730bfd01d8596bc45170d7
Bryan Collazo <bcollazo2010@gmail.com>
2025-08-11 11:26:20 -0400

    Final Fixes and Sylveon Notebook

--- examples/sylveon_deck_draw_speed.rs
+++ examples/sylveon_deck_draw_speed.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use uuid::Uuid;
 
 use deckgym::{
@@ -13,19 +13,17 @@ use deckgym::{
 
 /// Run with `cargo run --example sylveon_deck_draw_speed`
 fn main() {
-    let num_simulations = 1000;
-    let deck_a_path = "example_decks/solgaleo_sylveon.txt";
-    let deck_b_path = "example_decks/mewtwoex.txt";
+    let num_simulations = 1_000_000;
+    let deck_a_path = "example_decks/solgaleo_shiinotic.txt";
+    let deck_b_path = "example_decks/solgaleo_sylveon.txt";
     let player_codes = vec![PlayerCode::ER, PlayerCode::ER];
     initialize_logger(1);
     println!("This will count how fast the user cant draw cards from the deck.");
 
     let stats_collector = Box::new(StatsCollector::new());
-    let deckout_collector = Box::new(DeckOutCollector::new());
     let first_turn_seen_collector = Box::new(FirstTurnSeenCollector::new());
     let mut composite_handler = CompositeSimulationEventHandler::new();
     composite_handler.add_handler(stats_collector);
-    composite_handler.add_handler(deckout_collector);
     composite_handler.add_handler(first_turn_seen_collector);
     let mut simulation = Simulation::new(
         deck_a_path,
@@ -39,78 +37,18 @@ fn main() {
     simulation.run();
 }
 
-/// Simulation event handler that counts how fast players run out of cards in their decks.
-/// It generates a CSV file with (game_id, turn, actor, last_known_deck_size) tuples for each game.
-pub struct DeckOutCollector {
-    deck_sizes: HashMap<(Uuid, u8, usize), usize>, // (game_id, turn, actor) -> deck_size
-}
-
-impl DeckOutCollector {
-    pub fn new() -> Self {
-        Self {
-            deck_sizes: HashMap::new(),
-        }
-    }
-}
-
-impl SimulationEventHandler for DeckOutCollector {
-    fn on_action(
-        &mut self,
-        game_id: Uuid,
-        _state_before_action: &State,
-        actor: usize,
-        _playable_actions: &[Action],
-        _action: &Action,
-        state_after_action: &State,
-    ) {
-        let turn = state_after_action.turn_count;
-        let deck_size = state_after_action.decks[actor].cards.len();
-
-        // Since this is called multiple times per turn, writing idempotently
-        // we'll store the last known deck size for each game-turn-actor combination.
-        self.deck_sizes.insert((game_id, turn, actor), deck_size);
-    }
-
-    fn on_simulation_end(&mut self) {
-        println!("Deck sizes collected for all games. Writing to CSV...");
-
-        // Write the deck sizes to a CSV file
-        let absolute_path = std::env::current_dir()
-            .expect("Failed to get current directory")
-            .join("deck_sizes.csv");
-        let mut wtr = csv::Writer::from_path(&absolute_path).expect("Failed to create CSV writer");
-
-        // Convert to Vec and sort by (game_id, turn, actor)
-        let mut sorted_data: Vec<_> = self.deck_sizes.iter().collect();
-        sorted_data.sort_by_key(|((game_id, turn, actor), _)| (*game_id, *turn, *actor));
-
-        // Write header
-        wtr.write_record(&["game_id", "turn", "actor", "deck_size"])
-            .expect("Failed to write header to CSV");
-        for ((game_id, turn, actor), deck_size) in sorted_data {
-            wtr.write_record(&[
-                game_id.to_string(),
-                turn.to_string(),
-                actor.to_string(),
-                deck_size.to_string(),
-            ])
-            .expect("Failed to write record to CSV");
-        }
-        wtr.flush().expect("Failed to flush CSV writer");
-        println!("Deck sizes written to {}", absolute_path.display());
-    }
-}
-
 /// Simulation event handler that tracks the first turn a card is in play.
 pub struct FirstTurnSeenCollector {
     /// (game_id, actor, card_name) -> min_turn
     first_turn_seen: HashMap<(Uuid, usize, String), u8>,
+    game_ids: HashSet<Uuid>,
 }
 
 impl FirstTurnSeenCollector {
     pub fn new() -> Self {
         Self {
             first_turn_seen: HashMap::new(),
+            game_ids: HashSet::new(),
         }
     }
 }
@@ -131,16 +69,28 @@ impl SimulationEventHandler for FirstTurnSeenCollector {
                 let card_name = card.get_name();
                 let key = (game_id, actor, card_name.clone());
                 if !self.first_turn_seen.contains_key(&key) {
-                    println!(
-                        "Setting first turn seen for {:?} in game {}: turn {}",
-                        card_name.clone(),
-                        game_id,
-                        turn
-                    );
                     self.first_turn_seen.insert(key, turn);
                 }
             }
         }
+    }
+
+    // Every 100 games, give a progress update
+    fn on_game_end(
+        &mut self,
+        _game_id: Uuid,
+        _state: State,
+        _result: Option<deckgym::state::GameOutcome>,
+    ) {
+        if self.game_ids.len() % 1000 == 0 {
+            println!(
+                "Processed {} games so far. First turn seen: {}",
+                self.game_ids.len(),
+                self.first_turn_seen.len()
+            );
+        }
+        // Store the game ID to track progress
+        self.game_ids.insert(_game_id);
     }
 
     fn on_simulation_end(&mut self) {
