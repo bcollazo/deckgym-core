b47196a1d555f1242bebe25144f00d94b1f4ec9f
Bryan Collazo <bcollazo2010@gmail.com>
2025-09-29 22:38:44 -0400

    BugFix: Can always attack, even if in turn 1 (as long as there is energy)

--- src/move_generation/attacks.rs
+++ src/move_generation/attacks.rs
@@ -1,10 +1,6 @@
 use crate::{actions::SimpleAction, hooks::contains_energy, State};
 
 pub(crate) fn generate_attack_actions(state: &State) -> Vec<SimpleAction> {
-    if state.turn_count <= 1 {
-        return vec![];
-    }
-
     let current_player = state.current_player;
     let mut actions = Vec::new();
     if let Some(active_pokemon) = &state.in_play_pokemon[current_player][0] {
