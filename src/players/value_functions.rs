36d7017f355ec9cdf60b26a5be757dd3af24b8c9
Bryan Collazo <bcollazo2010@gmail.com>
2025-12-06 10:52:44 -0400

    Update EE

--- src/players/value_functions.rs
+++ src/players/value_functions.rs
@@ -33,17 +33,17 @@ impl ValueFunctionParams {
     pub const fn baseline() -> Self {
         Self {
             points: 10_000.0,
-            pokemon_value: 0.0,
-            hand_size: 100.0,
+            pokemon_value: 1.0,
+            hand_size: 1.0,
             deck_size: 1.0,
             active_retreat_cost: 1.0,
-            active_pokemon_online_score: 100.0,
+            active_pokemon_online_score: 500.0,
             active_safety: 1.0,
             active_has_tool: 10.0,
             is_winner: 100_000.0,
             turns_until_opponent_wins: 100.0,
-            online_pokemon_count: 10.0,
-            energy_distance_to_online: 10.0,
+            online_pokemon_count: 0.0,
+            energy_distance_to_online: 0.0,
         }
     }
 
