92eff4945bc12bc4b815d54d76264f6e3e673238
Bryan Collazo <bcollazo2010@gmail.com>
2026-04-27 11:06:02 -0400

    Fix pokemon test module formatting

--- tests/pokemon.rs
+++ tests/pokemon.rs
@@ -46,10 +46,10 @@ mod rampardos_head_smash_test;
 mod shinx_hide_test;
 #[path = "pokemon/sunflora_quick_grow_beam_test.rs"]
 mod sunflora_quick_grow_beam_test;
-#[path = "pokemon/vaporeon_ex_test.rs"]
-mod vaporeon_ex_test;
 #[path = "pokemon/tepig_stoke_test.rs"]
 mod tepig_stoke_test;
+#[path = "pokemon/vaporeon_ex_test.rs"]
+mod vaporeon_ex_test;
 #[path = "pokemon/vulpix_tail_whip_test.rs"]
 mod vulpix_tail_whip_test;
 #[path = "pokemon/xerneas_geoburst_test.rs"]
